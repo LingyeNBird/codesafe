ddd5ed6a1a46a6338ee0c112c6151b104d352acd
lingye <2389156934@qq.com>
2026-09-20 10:03:07 -0700

    chore: 移除误提交的 tmp_gen 测试目录并加入 gitignore

--- tmp_gen/main.go
+++
@@ -1,9 +0,0 @@
-package main
-import ("fmt"; "strings")
-func main() {
-	var b strings.Builder
-	for i := 0; i < 3000; i++ {
-		fmt.Fprintf(&b, "// line %d: the quick brown fox jumps over the lazy dog and keeps running\n", i)
-	}
-	fmt.Print(b.String())
-}
